625a2670cb5b3d01374d35645eebae9d0fb8c886
Aaron <radees.24.16@gmail.com>
2022-04-17 12:23:19 -0500

    exam practice javaScript

---
+++ clase_1_examen.js
@@ -0,0 +1,12 @@
+var nombre="Aarón";
+var apellidos="Delgado";
+var nombre_de_usuario_en_platzi="AaronDel";
+var edad=24;
+var correo_electronico="aaron@gmail.com";
+var mayor_de_edad=true;
+var dinero_ahorrado=123.23;
+var deudas=2345.31;
+
+var nombre_completo=nombre+apellidos;
+document.write(nombre);
+
